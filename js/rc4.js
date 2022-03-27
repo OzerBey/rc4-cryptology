ee84c16d7f3a77fb484329296d095d25a1a1133b
OzerBey <yasinozeriletisim@gmail.com>
2022-03-27 11:49:34 +0300

    Complete step2 for rc4

--- js/rc4.js
+++ js/rc4.js
@@ -6,6 +6,22 @@ const rc4 = (str, key) => {
     s[i] = i;
     k[i] = key.charCodeAt(i % key.length);
   }
+
+  //step 2
+  j = 0;
+  for (let i = 0; i < 255; i++) {
+    let temp;
+    j = (j + s[i] + k[i]) % 256;
+    temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+  }
 };
 
 //tests
+rc4("hello world", "rc4-key@");
+
+let data = "hello world";
+let charCode = data.charCodeAt(0);
+console.log("charCode", charCode);
+console.log("fromCharCode", String.fromCharCode(104));
