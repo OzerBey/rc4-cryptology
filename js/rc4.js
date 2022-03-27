be693ee77144bccb51c58cca4fa393b259c8b862
OzerBey <yasinozeriletisim@gmail.com>
2022-03-27 13:26:28 +0300

    Complete key genarator step

--- js/rc4.js
+++ js/rc4.js
@@ -11,16 +11,35 @@ const rc4 = (str, key) => {
   for (let i = 0; i < 255; i++) {
     let temp;
     j = (j + s[i] + k[i]) % 256;
+    // console.log(i, j);
     temp = s[i];
     s[i] = s[j];
     s[j] = temp;
   }
+
+  //step 3 : Keystream generation
+
+  i = 0;
+  j = 0;
+  let result = "";
+
+  for (let y = 0; y < str.length; y++) {
+    i = (i + 1) % 256;
+    j = (j + s[i]) % 256;
+    //swap process
+    let temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+    key = s[(s[i] + s[j]) % 256];
+    result += String.fromCharCode(str.charCodeAt(y) ^ key);
+  }
+  return result;
 };
 
 //tests
-rc4("hello world", "rc4-key@");
-
+let result = rc4("Hello World", "rc4-key@"); //ascii
+console.log("result of Rc4 algorithm", result);
 let data = "hello world";
 let charCode = data.charCodeAt(0);
-console.log("charCode", charCode);
-console.log("fromCharCode", String.fromCharCode(104));
+console.log("charCode", charCode); //104
+console.log("fromCharCode", String.fromCharCode(104)); //h
